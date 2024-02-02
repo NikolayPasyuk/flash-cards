8498991e5bdb0fb1df7d4c38441cceb7fef1f1a6
NikolayPasyuk <kolyan-1812@mail.ru>
2024-02-02 03:53:30 +0300

    feat: add themes for preview

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import type { Preview } from "@storybook/react";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/700.css";
 import "../src/styles/index.scss";
+import { themes } from "@storybook/theming";
 
 const preview: Preview = {
   parameters: {
@@ -9,8 +10,20 @@ const preview: Preview = {
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
+        date: /Date$/,
       },
+    },
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      default: "dark",
+      values: [
+        {
+          name: "dark",
+          value: "#000",
+        },
+      ],
     },
   },
 };
