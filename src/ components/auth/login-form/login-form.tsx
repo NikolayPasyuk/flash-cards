6d2e7161e2a16bf04fed39ecca21b2b39347ee5c
NikolayPasyuk <kolyan-1812@mail.ru>
2024-02-17 20:54:41 +0300

    feat: add component login-form

---
+++ src/ components/auth/login-form/login-form.tsx
@@ -0,0 +1,92 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "../../ui/button";
+import { Card } from "../../ui/card";
+import { ControlledCheckbox, ControlledTextField } from "../../ui/controlled";
+import { Typography } from "../../ui/typography";
+
+import s from "./login-form.module.scss";
+
+const schema = z.object({
+  email: z
+    .string()
+    .trim()
+    .email("Invalid email address")
+    .refine((value) => value.trim() !== "", {
+      message: "Enter email",
+    }),
+  password: z
+    .string()
+    .trim()
+    .refine((value) => value.trim() !== "", { message: "Enter password" })
+    .refine((value) => value.length >= 8, {
+      message: "Password must be at least 8 characters",
+    }),
+  rememberMe: z.boolean(),
+});
+
+type FormType = z.infer<typeof schema>;
+type Props = {
+  onSubmit: (data: FormType) => void;
+};
+export const LoginForm = (props: Props) => {
+  const { handleSubmit, control } = useForm<FormType>({
+    resolver: zodResolver(schema),
+    mode: "onSubmit",
+    defaultValues: {
+      email: "",
+      password: "",
+      rememberMe: false,
+    },
+  });
+
+  const handleFormSubmitted = handleSubmit(props.onSubmit);
+
+  return (
+    <>
+      <Card className={s.card}>
+        <Typography variant="large" as={"h1"}>
+          Sign in
+        </Typography>
+        <form onSubmit={handleFormSubmitted}>
+          <ControlledTextField
+            label="Email"
+            name={"email"}
+            control={control}
+            containerProps={{ className: s.textField }}
+          />
+          <ControlledTextField
+            label="Password"
+            type={"password"}
+            name={"password"}
+            control={control}
+            containerProps={{ className: s.textField }}
+          />
+          <ControlledCheckbox
+            label={"Remember me"}
+            name={"rememberMe"}
+            control={control}
+            className={s.checkbox}
+            position={"left"}
+          />
+
+          <Typography variant="body2" className={s.forgotPassword}>
+            Forgot password?
+          </Typography>
+
+          <Button type={"submit"} fullWidth>
+            Submit
+          </Button>
+        </form>
+        <Typography variant="body2" className={s.noAccount}>
+          Don't have an account?
+        </Typography>
+        <Typography variant="link1" className={s.signUpLink}>
+          Sign Up
+        </Typography>
+      </Card>
+    </>
+  );
+};
