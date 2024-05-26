95079221591efcba88152e1b1f2086c0b73f8849
NikolayPasyuk <kolyan-1812@mail.ru>
2024-05-27 01:33:47 +0300

    feat: add component TableHeader

---
+++ src/ components/ui/table/table-header.tsx
@@ -0,0 +1,84 @@
+import { FC, MouseEventHandler } from "react";
+
+import { clsx } from "clsx";
+
+import { ArrowDown } from "../../../assets/icons/arrow-down";
+import { ArrowUp } from "../../../assets/icons/arrow-up";
+import { Typography } from "../typography";
+
+import { Table } from "./table";
+import s from "./table.module.scss";
+import { Column } from "./types.ts";
+
+export type Sort = {
+  key: string;
+  direction: "asc" | "desc";
+} | null;
+
+type Props = {
+  columns: Column[];
+  sort: Sort;
+  onSort: (sort: Sort) => void;
+};
+
+const dataAttributes = {
+  sortable: "data-sortable",
+  name: "data-name",
+} as const;
+
+export const TableHeader: FC<Props> = ({
+  columns,
+  sort,
+  onSort,
+  ...restProps
+}) => {
+  const handleSorting: MouseEventHandler<HTMLTableRowElement> = (e) => {
+    if (!(e.target instanceof HTMLSpanElement)) return;
+
+    const isSortable = e.target.getAttribute(dataAttributes.sortable);
+    const key = e.target.getAttribute(dataAttributes.name);
+
+    if (!isSortable || isSortable !== "true") return;
+
+    if (key === null) throw new Error("No data-name found!");
+
+    if (key !== sort?.key) {
+      return onSort({ key, direction: "asc" });
+    }
+
+    if (sort.direction === "asc") {
+      return onSort({ key, direction: "desc" });
+    }
+
+    onSort(null);
+  };
+
+  return (
+    <Table.Head {...restProps}>
+      <Table.Row onClick={handleSorting}>
+        {columns.map((column) => {
+          const showSort = column.isSortable && sort && sort.key === column.key;
+
+          return (
+            <Table.HeadCell key={column.title}>
+              <Typography
+                variant={"subtitle2"}
+                className={clsx(column.isSortable, s.sortLabel)}
+                as={"span"}
+                {...{
+                  [dataAttributes.sortable]: column.isSortable,
+                  [dataAttributes.name]: column.key,
+                }}
+              >
+                {column.title}
+                {showSort && (
+                  <>{sort.direction === "asc" ? <ArrowUp /> : <ArrowDown />}</>
+                )}
+              </Typography>
+            </Table.HeadCell>
+          );
+        })}
+      </Table.Row>
+    </Table.Head>
+  );
+};
