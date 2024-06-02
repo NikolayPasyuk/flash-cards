b49a9d75ce5dcfa6f45e5b50c125570fcfeb63ff
NikolayPasyuk <kolyan-1812@mail.ru>
2024-06-02 17:52:14 +0300

    feat: added function appendDataToFormData

---
+++ src/shared/utils/append-data-to-form-data.ts
@@ -0,0 +1,18 @@
+export const appendDataToFormData = (data: Data) => {
+  const formData = new FormData();
+
+  for (const key in data) {
+    const value = data[key];
+
+    // Преобразование значений boolean в строки
+    if (typeof value === "boolean") {
+      formData.append(key, value ? "true" : "false");
+    } else {
+      formData.append(key, value);
+    }
+  }
+
+  return formData;
+};
+
+type Data = Record<string, string | Blob | boolean>;
