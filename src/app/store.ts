0bea5bba5b9f578eb3ef60f4f7b979000c56421e
NikolayPasyuk <kolyan-1812@mail.ru>
2024-07-10 18:23:19 +0300

    feat: configured Redux store setup

---
+++ src/app/store.ts
@@ -0,0 +1,20 @@
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+
+import { baseApi } from "../services/base-api.ts";
+
+export const store = configureStore({
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
