8c4cc0cf8165c15126e7369b0c0ca2f002e2bb59
NikolayPasyuk <kolyan-1812@mail.ru>
2024-07-11 17:01:32 +0300

    feat(api): added logout mutation with cache invalidation and local state updates

--- src/services/auth/auth-endpoints.ts
+++ src/services/auth/auth-endpoints.ts
@@ -2,6 +2,7 @@ import { Nullable } from "../../types/common.types.ts";
 import { baseApi } from "../base-api.ts";
 
 import { MeResponse } from "./types.ts";
+import { setClearFilter } from "@/services/decks/deck-query-params.slice.ts";
 
 export const authEndpoints = baseApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -13,7 +14,28 @@ export const authEndpoints = baseApi.injectEndpoints({
       }),
       providesTags: ["Me"],
     }),
+    logout: builder.mutation<unknown, void>({
+      query: () => ({
+        url: `auth/logout`,
+        method: "POST",
+      }),
+      async onQueryStarted(_, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          authEndpoints.util.updateQueryData("me", undefined, () => {
+            return null;
+          }),
+        );
+
+        dispatch(setClearFilter());
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+      invalidatesTags: ["Me"],
+    }),
   }),
 });
 
-export const { useMeQuery } = authEndpoints;
+export const { useMeQuery, useLogoutMutation } = authEndpoints;
