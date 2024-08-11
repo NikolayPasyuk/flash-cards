45a6e3232ebbca2dc5d7f784398fdfd0d850189b
NikolayPasyuk <kolyan-1812@mail.ru>
2024-08-11 16:12:36 +0300

    feat: set up root rendering with Redux and Toast notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "@/App";
-import "./styles/index.scss";
+import { createRoot } from "react-dom/client";
+
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/700.css";
+import "./styles/index.scss";
+import { StrictMode } from "react";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { store } from "./app/store.ts";
+import { App } from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Provider store={store}>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <App />
+    </Provider>
+  </StrictMode>,
 );
