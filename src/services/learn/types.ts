af195db5ed0490b293c58dc84a0868806ecd396c
NikolayPasyuk <kolyan-1812@mail.ru>
2025-01-02 16:54:18 +0300

    feat: added SaveGradeArgs type for saving card grades

--- src/services/learn/types.ts
+++ src/services/learn/types.ts
@@ -1,3 +1,10 @@
 import { Card } from "../cards/types.ts";
+import { Deck } from "../decks/types.ts";
 
 export type LearnDeckResponse = Omit<Card, "userId">;
+
+export type SaveGradeArgs = {
+  deckId: Deck["id"];
+  cardId: Card["id"];
+  grade: number;
+};
