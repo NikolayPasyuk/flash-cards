c35d04e1da1b445e622f125953f0c5786f97e3df
NikolayPasyuk <kolyan-1812@mail.ru>
2025-01-04 21:17:35 +0300

    feat: added LearnPage with grading functionality and data display

---
+++ src/pages/learn-page/learn-page.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import { Learn } from "../../ components/learn/learn.tsx";
+import { Container } from "../../ components/ui/container";
+import { GoBack } from "../../ components/ui/go-back";
+import { useGetDeckByIdQuery } from "../../services/decks/decks-endpoints.ts";
+import {
+  useLearnDeckQuery,
+  useSaveGradeMutation,
+} from "../../services/learn/learn-endpoints.ts";
+
+export const LearnPage = () => {
+  const { deckId } = useParams<{ deckId: string }>();
+
+  const { data: deckData } = useGetDeckByIdQuery({
+    id: deckId || "",
+  });
+  const { data: cardData } = useLearnDeckQuery({ id: deckId || "" });
+  const [saveGrade] = useSaveGradeMutation();
+
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  const handleSaveGrade = async (grade: number) => {
+    await saveGrade({ grade, deckId: deckId || "", cardId: cardData?.id || "" })
+      .unwrap()
+      .then(() => {
+        setShowAnswer(false);
+      })
+      .catch((err) => {
+        const error = err as { data: { message: string } };
+
+        toast.error(error.data.message);
+      });
+  };
+
+  return (
+    <Container>
+      <GoBack title={"Back to Deck Page"} />
+      {deckData && cardData && (
+        <Learn
+          setShowAnswer={setShowAnswer}
+          showAnswer={showAnswer}
+          shots={cardData.shots}
+          handleSaveGrade={handleSaveGrade}
+          deckName={deckData.name}
+          question={cardData.question}
+          answer={cardData.answer}
+        />
+      )}
+    </Container>
+  );
+};
